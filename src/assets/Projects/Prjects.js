0b08e849a296fe79889603e2e68f05db6246684a
DMC <dorykahale@gmail.com>
2021-03-12 18:58:18 -0600

    add assets, projects, images

---
+++ src/assets/Projects/Prjects.js
@@ -0,0 +1,59 @@
+import mixlive from "../images/mixlive-main.png";
+import quilava from "../images/quilava.png";
+import scheduler from "../images/scheduler-main.png";
+import recipe from "../images/project1-main.png";
+import EatDaBurger from "../images/EatDaBurger.png";
+import pwgen from "../images/pwgen-main.png";
+
+const projectsArr = [
+ {
+    "id": 1,
+    "name": "Mixlive",
+    "image": mixlive,
+    "description": "Streaming for Artists & DJs",
+    "repo": "",
+    "deployed": ""
+  },
+  {
+    "id": 2,
+    "name": "Quilava",
+    "image": quilava,
+    "description": "",
+    "repo": "",
+    "deployed": ""
+  },
+  {
+    "id": 3,
+    "name": "Scheduler",
+    "image": scheduler,
+    "description": "",
+    "repo": "",
+    "deployed": ""
+  },
+  {
+    "id": 4,
+    "name": "Recipe Saver",
+    "image": recipe,
+    "description": "",
+    "repo": "",
+    "deployed": ""
+  },
+  {
+    "id": 5,
+    "name": "Eat Da Burger",
+    "image": EatDaBurger,
+    "description": "Not for those on a diet",
+    "repo": "",
+    "deployed": ""
+  },
+  {
+    "id": 6,
+    "name": "Password Generator",
+    "image": pwgen,
+    "description": "",
+    "repo": "",
+    "deployed": ""
+  },
+]
+
+export default projectsArr;
